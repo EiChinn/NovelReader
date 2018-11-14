ee41fb07954066d033ed5bbbab2e5f37fa763621
Ei Chinn <eichinn77@gmail.com>
2018-11-14 14:04:34 +0800

    依赖Stetho用于查看数据库；升级LeakCanary版本

--- app/src/main/java/com/example/newbiechen/ireader/App.java
+++ app/src/main/java/com/example/newbiechen/ireader/App.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import com.example.newbiechen.ireader.service.DownloadService;
+import com.facebook.stetho.Stetho;
 import com.squareup.leakcanary.LeakCanary;
 
 /**
@@ -21,9 +22,16 @@ public class App extends Application {
         startService(new Intent(getContext(), DownloadService.class));
 
         // 初始化内存分析工具
-        if (!LeakCanary.isInAnalyzerProcess(this)) {
-            LeakCanary.install(this);
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
         }
+        LeakCanary.install(this);
+
+//        BlockCanary.install(this, new AppBlockCanaryContext()).start();
+
+        Stetho.initializeWithDefaults(this);
     }
 
     public static Context getContext(){
