0692dd8d6e1efc217b7f65920eafe17b2e9fc313
EiChinn <eichinn77@gmail.com>
2018-11-18 12:08:02 +0800

    修复部分内存泄露问题

--- app/src/main/java/com/example/newbiechen/ireader/ui/activity/ChangeSourceActivity.java
+++ app/src/main/java/com/example/newbiechen/ireader/ui/activity/ChangeSourceActivity.java
@@ -98,6 +98,7 @@ public class ChangeSourceActivity extends BaseMVPActivity<ChangeSourceContract.P
 	@Override
 	protected void onDestroy() {
 		rv_book_sources.setAdapter(null);
+		adapter.setOnItemClickListener(null);
 		super.onDestroy();
 	}
 }
