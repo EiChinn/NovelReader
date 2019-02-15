211ee6916237d71453bb25fdee8a737fe6234cdf
Ei Chinn <eichinn77@gmail.com>
2019-02-15 11:36:55 +0800

    migrate to Kotlin

---
+++ app/src/main/java/com/example/newbiechen/ireader/widget/animation/NonePageAnim.kt
@@ -0,0 +1,23 @@
+package com.example.newbiechen.ireader.widget.animation
+
+import android.graphics.Canvas
+import android.view.View
+
+class NonePageAnim(w: Int, h: Int, view: View, listener: OnPageChangeListener) : HorizonPageAnim(w, h, view, listener) {
+    override fun drawStatic(canvas: Canvas) {
+        if (isCancel) {
+            canvas.drawBitmap(mCurBitmap, 0f, 0f, null)
+        } else {
+            canvas.drawBitmap(mNextBitmap, 0f, 0f, null)
+        }
+    }
+
+    override fun drawMove(canvas: Canvas) {
+        if (isCancel) {
+            canvas.drawBitmap(mCurBitmap, 0f, 0f, null)
+        } else {
+            canvas.drawBitmap(mNextBitmap, 0f, 0f, null)
+        }
+    }
+
+}
